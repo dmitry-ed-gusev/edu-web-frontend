5755b1c18257010caa44cd400610136d8170ad2a
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2021-01-02 22:26:03 +0100

    Minor improvements for project init script.

--- setup_project.sh
+++ setup_project.sh
@@ -12,22 +12,16 @@
 BASE_SITE_DIR=$1
 
 if [[ -z ${BASE_SITE_DIR} ]]; then
-    echo "Provided empty project name! ${BASE_SITE_DIR}"
+    echo "Provided empty project / module name! ${BASE_SITE_DIR}"
     exit -1
 fi
 
 echo "Creating project module: ${BASE_SITE_DIR}"
 
+# -- create directories structure
 mkdir -p ${BASE_SITE_DIR}/dist
 mkdir -p ${BASE_SITE_DIR}/src/{favicon,html,fonts,img,js,scss,uploads}
 mkdir -p ${BASE_SITE_DIR}/src/html/{includes,views}
-#mkdir -p "${BASE_SITE_DIR}/src/fonts"
-#mkdir -p "${BASE_SITE_DIR}/src/html/includes"
-#mkdir -p "${BASE_SITE_DIR}/src/html/views"
-#mkdir -p "${BASE_SITE_DIR}/src/img"
-#mkdir -p "${BASE_SITE_DIR}/src/js"
-#mkdir -p "${BASE_SITE_DIR}/src/scss"
-#mkdir -p "${BASE_SITE_DIR}/src/uploads"
 
 # -- init node project and generates package.json file
 cd ${BASE_SITE_DIR}
@@ -36,8 +30,8 @@ npm init
 # -- install webpack (the latest) + CLI + dev server and save it as development dependency
 npm install webpack webpack-cli webpack-dev-server clean-webpack-plugin del-cli --save-dev
 
-# -- install additional webpack modules
-#npm install copy-webpack-plugin html-webpack-plugin --save-dev
+# -- install additional webpack modules - development dependencies
+npm install copy-webpack-plugin html-webpack-plugin --save-dev
 
 # -- install babel - JavaScript compiler - development dependency
 npm install babel-core babel-preset-env babel-loader --save-dev
@@ -49,7 +43,7 @@ npm install jquery --save
 #npm install node-sass sass-loader css-loader mini-css-extract-plugin --save-dev
 
 # -- install bootstrap and required dependencies
-#npm install bootstrap jquery popper.js --save
+#npm install bootstrap popper.js --save
 
 # -- go back to the root directory
 cd ..
