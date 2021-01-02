0f7cabee9739f9a44e9e9644bd3e9cd94f0f2654
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2021-01-02 22:10:56 +0100

    Added a lot of files from sample project - build is broken...

--- simple-site/src/js/index.js
+++ simple-site/src/js/index.js
@@ -2,6 +2,16 @@ import moment from 'moment';
 import jQuery from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
+import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
 
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
@@ -12,3 +22,9 @@ console.log(`Hello ${name}, how are you ${time}?`);
 jQuery(function() {
     jQuery('body').css('color', 'blue');
 });
+
+// bootstrap's command
+document.body.style.color = "blue";
+
+// call component() function
+document.body.appendChild(component());
--- simple-site/webpack.config.js
+++ simple-site/webpack.config.js
@@ -1,28 +1,120 @@
 const path = require('path');
+const fs = require("fs");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+//const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+//const cssnano = require("cssnano");
 
-module.exports = {
-    entry: [
-        './src/js/index.js' // source js files (path in the current folder)
-    ],
+function generateHtmlPlugins(templateDir) {
+    const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+    return templateFiles.map((item) => {
+        const parts = item.split(".");
+        const name = parts[0];
+        const extension = parts[1];
+        return new HtmlWebpackPlugin({
+            filename: `${name}.html`,
+            template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+            inject: false,
+        });
+    });
+}
+
+const htmlPlugins = generateHtmlPlugins("./src/html/views");
+
+const config = {
+    entry: ["./src/js/index.js", "./src/scss/style.scss"], // source js files (relative path in the current project folder)
     output: {
-        filename: './js/bundle.js' // compiled / assembled js bundle (path in the folder <dist>)
+        path: path.resolve(__dirname, "dist"),
+        filename: "./js/bundle.js", // compiled / assembled js bundle (relative path in the folder <./dist>)
     },
     devtool: "source-map", // development tools - source mapping for debug
+    mode: "production",
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                //sourceMap: true,
+                extractComments: true,
+            }),
+        ],
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src/js'), // absolute path
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, "src/scss"),
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            url: false,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.html$/,
+                include: path.resolve(__dirname, "src/html/includes"),
+                use: ["raw-loader"],
+            },
+        ],
     },
     plugins: [
-    ]
+        new MiniCssExtractPlugin({
+            filename: "./css/style.bundle.css",
+        }),
+
+        /*
+        new OptimizeCSSAssetsPlugin({
+            cssProcessor: cssnano,
+            cssProcessorOptions: {
+                map: { inline: false, annotation: true },
+                discardComments: { removeAll: true },
+                safe: true,
+            },
+            canPrint: true,
+        }),
+        */
+
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "./src/fonts",
+                    to: "./fonts",
+                },
+                {
+                    from: "./src/favicon",
+                    to: "./favicon",
+                },
+                {
+                    from: "./src/img",
+                    to: "./img",
+                },
+                {
+                    from: "./src/uploads",
+                    to: "./uploads",
+                },
+            ],
+        }),
+    ].concat(htmlPlugins),
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === "production") {
+        config.plugins.push(new CleanWebpackPlugin());
+    }
+    return config;
 };
